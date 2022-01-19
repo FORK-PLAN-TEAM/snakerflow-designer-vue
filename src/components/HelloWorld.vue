3be0f9e44304b7a320e7ad7f195e47e51c2a3076
mldong <mldong666@qq.com>
2022-01-19 20:49:31 +0800

    初次提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,113 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div id="mountNode"></div>
   </div>
 </template>
 
 <script>
+import G6 from '@antv/g6'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted () {
+    const data = {
+      // 点集
+      nodes: [{
+        id: 'start1',
+        label: '开始',
+        type: 'begin',
+        x: 24,
+        y: 124
+      }, {
+        id: 'end1',
+        label: '结束',
+        type: 'end',
+        x: 570,
+        y: 124
+      }, {
+        id: 'apply',
+        shape: 'customNode',
+        label: '请假申请',
+        x: 117,
+        y: 122
+      }, {
+        id: 'approveDept',
+        label: '部门经理审批',
+        x: 272,
+        y: 122
+      }, {
+        id: 'decision1',
+        label: '决策',
+        x: 426,
+        y: 124
+      }, {
+        id: 'approveBoss',
+        label: '总经理审批',
+        x: 404,
+        y: 231
+      }],
+      // 边集
+      edges: [{
+        source: 'start1', // String，必须，起始点 id
+        target: 'apply' // String，必须，目标点 id
+      }, {
+        source: 'apply',
+        target: 'approveDept'
+      }, {
+        source: 'approveDept',
+        target: 'decision1'
+      }, {
+        source: 'decision1',
+        target: 'approveBoss'
+      }, {
+        source: 'approveBoss',
+        target: 'end1',
+        label: '>2天'
+      }, {
+        source: 'decision1',
+        target: 'end1',
+        label: '<=2天'
+      }]
+    }
+    G6.registerNode('customNode', {
+      anchor: [
+        [0.5, 1], // 底边中点
+        [0.5, 0] // 上边中点
+      ]
+    })
+
+    const graph = new G6.Graph({
+      container: 'mountNode',
+      width: 800,
+      height: 800,
+      defaultNode: {
+        shape: 'rect',
+        style: {
+          fill: '#fff',
+          fontSize: 14
+        }
+      },
+      defaultEdge: {
+        shape: 'line-with-arrow',
+        style: {
+          endArrow: true,
+          lineWidth: 2,
+          stroke: '#ccc'
+        }
+      }
+    })
+    graph.data(data) // 读取 Step 2 中的数据源到图上
+    graph.render() // 渲染图
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#mountNode{
+  border: 1px solid red;
 }
 </style>
